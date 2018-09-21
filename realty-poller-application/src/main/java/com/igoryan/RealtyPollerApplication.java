84b377f4ec32ab62816bd3ec7c8481bc999af826
igya0716 <igor.yadrov@netcracker.com>
2018-09-21 16:44:03 +0300

    Initial commit

---
+++ realty-poller-application/src/main/java/com/igoryan/RealtyPollerApplication.java
@@ -0,0 +1,10 @@
+package com.igoryan;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtyPollerApplication {
+
+  public static void main(String[] args) {
+  }
+}
